961699d58512b024790ebe593ca14a754c158134
Yoonwon Choi <one20470@gmail.com>
2024-03-15 10:48:16 -0700

    worked on responsiveness for tablet and desktop for warehouse detail component

--- src/components/InventoriesList/InventoriesList.js
+++ src/components/InventoriesList/InventoriesList.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { StatusTag } from "../StatusTag/StatusTag";
+// import { StatusTag } from "../StatusTag/StatusTag";
+import Status from "../Status/Status";
 import "./InventoriesList.scss";
 import AttributeLink from "../AttributeLink/AttributeLink";
 import DeleteButton from "../DeleteButton/DeleteButton";
@@ -54,7 +55,8 @@ export function InventoriesListItem({
           <div className="inventories-list__col">
             <div className="inventories-list__sub-item">
               <h4 className="inventories-list__label">STATUS</h4>
-              <StatusTag status={status} />
+              {/* <StatusTag status={status} /> */}
+              <Status text={status} />
             </div>
             <div className="inventories-list__sub-item">
               <h4 className="inventories-list__label">QTY</h4>
--- src/pages/WarehouseDetails/WarehouseDetails.js
+++ src/pages/WarehouseDetails/WarehouseDetails.js
@@ -1,5 +1,4 @@
 import "./WarehouseDetails.scss";
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -7,7 +6,6 @@ import ArrowBack from "../../components/ArrowBack/ArrowBack";
 import CircleEdit from "../../components/CircleEditButton/CircleEditButton";
 import { BASE_URL } from "../../utils/constant-variables";
 import { InventoriesList } from "../../components/InventoriesList/InventoriesList";
-import EditButtonWithIcon from "../../components/EditButtonWithIcon/EditButtonWithIcon";
 
 export const WarehouseDetails = () => {
   const [warehouse, setWarehouse] = useState({});
@@ -51,9 +49,7 @@ export const WarehouseDetails = () => {
     <div className="warehouse-details__center-wrap">
       <section className="warehouse-details__header">
         <div className="warehouse-details__header-container">
-          <Link to={"/"}>
-            <ArrowBack />
-          </Link>
+          <ArrowBack to={"/"} />
           <div className="warehouse-details__header-name">
             {warehouse.warehouse_name}
           </div>
@@ -62,15 +58,20 @@ export const WarehouseDetails = () => {
           <CircleEdit />
         </div>
         <div className="warehouse-details__header-edit-tablet">
-         <EditButtonWithIcon />
+          <CircleEdit />
         </div>
       </section>
       <hr className="warehouse-details__divider1" />
       <section className="warehouse-details__container">
         <div className="warehouse-details__sub-item-address">
           <h4 className="warehouse-details__label">WAREHOUSE ADDRESS:</h4>
-          <div className="warehouse-details__value">{warehouse.address}</div>
+          <div className="warehouse-details__value">
+            {`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}
+          </div>
         </div>
+
+        <hr className="warehouse-details__section-divider" />
+
         <div className="warehouse-details__container-row">
           <div className="warehouse-details__sub-item">
             <h4 className="warehouse-details__label">CONTACT NAME:</h4>
