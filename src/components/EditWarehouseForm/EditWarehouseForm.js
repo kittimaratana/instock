070a98e91e8339a885fad4acaeb4b8477aafd447
Prome-ISA <l1109699036@gmail.com>
2024-03-16 19:51:37 -0400

    updated styling and validations

--- src/components/EditWarehouseForm/EditWarehouseForm.js
+++ src/components/EditWarehouseForm/EditWarehouseForm.js
@@ -1,13 +1,14 @@
-import "./AddWarehouseForm.scss";
+import "./EditWarehouseForm.scss";
 import { BASE_URL } from "../../utils/constant-variables";
 import EmptyField from "../EmptyField/EmptyField";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import ArrowBack from "../ArrowBack/ArrowBack";
 
-const CreateNewWarehouse = () => {
+const EditWarehouseForm = () => {
+    const { warehouse_Id } = useParams();
     const [warehouse_name, setWarehouse_name] = useState('');
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
@@ -17,7 +18,6 @@ const CreateNewWarehouse = () => {
     const [contact_phone, setContact_phone] = useState('');
     const [contact_email, setContact_email] = useState('');
 
-    const [hasSubmit, setHasSubmit] = useState(false);
     const [isPending, setIsPending] = useState(false);
     const [submitSuccess, setSubmitSuccess] = useState(false);
     const navigate = useNavigate();
@@ -25,9 +25,9 @@ const CreateNewWarehouse = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setHasSubmit(true);
-        // Convert the phone number to the desired format
-        const formattedPhone = formatPhoneNumber(contact_phone);
+
+         // Convert the phone number to the desired format
+         const formattedPhone = formatPhoneNumber(contact_phone);
         // Validation checks
         if (
             !warehouse_name ||
@@ -66,7 +66,7 @@ const CreateNewWarehouse = () => {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(newWarehouse)
             }).then(setIsPending(false),
-            );
+                   );
 
             if (response.ok) {
                 // Handle successful add
@@ -112,14 +112,6 @@ const CreateNewWarehouse = () => {
 
     };
 
-    let phoneErrorMessage = null;
-
-    const phoneRegex = /^\+\d{1,2}\s\(\d{3}\)\s\d{3}-\d{4}$/
-    if (contact_phone === "" || !phoneRegex.test(formatPhoneNumber(contact_phone))) {
-        
-        phoneErrorMessage = <EmptyField message="Please provide valid number" />;
-    } 
-
 
 
 
@@ -144,11 +136,10 @@ const CreateNewWarehouse = () => {
                             className="add-warehouse__form-input"
                             placeholder="Warehouse Name"
                             type="text"
-
+                            required
                             value={warehouse_name}
                             onChange={(e) => setWarehouse_name(e.target.value)}
                         />
-                        {warehouse_name === "" && hasSubmit && <EmptyField />}
 
 
                         <label className="add-warehouse__form-label">Address:  </label>
@@ -156,11 +147,10 @@ const CreateNewWarehouse = () => {
                             className="add-warehouse__form-input"
                             placeholder="Street address"
                             type="text"
-
+                            required
                             value={address}
                             onChange={(e) => setAddress(e.target.value)}
                         />
-                        {address === "" && hasSubmit && <EmptyField />}
 
 
                         <label className="add-warehouse__form-label">City:</label>
@@ -168,11 +158,10 @@ const CreateNewWarehouse = () => {
                             className="add-warehouse__form-input"
                             placeholder="City"
                             type="text"
-
+                            required
                             value={city}
                             onChange={(e) => setCity(e.target.value)}
                         />
-                        {city === "" && hasSubmit && <EmptyField />}
 
 
                         <label className="add-warehouse__form-label">Country: </label>
@@ -180,67 +169,61 @@ const CreateNewWarehouse = () => {
                             className="add-warehouse__form-input"
                             placeholder="Country"
                             type="text"
-
+                            required
                             value={country}
                             onChange={(e) => setCountry(e.target.value)}
                         />
-                        {country === "" && hasSubmit && <EmptyField />}
 
                     </div>
 
-                    <hr className="add-warehouse__divider add-warehouse__divider--between-components" />
+                    <hr className="add-warehosue__divider add-warehosue__divider--between-components" />
 
                     <div className="add-warehouse__details">
 
                         <h2>Contact Details</h2>
-                        <label className="add-warehouse__form-label" >Contact Name: </label>
-                        <input
-                            className="add-warehouse__form-input"
-                            placeholder="Contact Name"
-                            type="text"
+                        <label className="add-warehouse__form-label" >Contact Name:
+                            <input
+                                className="add-warehouse__form-input"
+                                placeholder="Contact Name"
+                                type="text"
+                                required
+                                value={contact_name}
+                                onChange={(e) => setContact_name(e.target.value)}
+                            />
+                        </label>
 
-                            value={contact_name}
-                            onChange={(e) => setContact_name(e.target.value)}
-                        />
-                        {contact_name === "" && hasSubmit && <EmptyField />}
+                        <label className="add-warehouse__form-label">Contact Position:
+                            <input
+                                className="add-warehouse__form-input"
+                                placeholder="Contact Position"
+                                type="text"
+                                required
+                                value={contact_position}
+                                onChange={(e) => setContact_position(e.target.value)}
+                            />
+                        </label>
 
+                        <label className="add-warehouse__form-label">Phone Number:
+                            <input
+                                className="add-warehouse__form-input"
+                                placeholder="Phone Number"
+                                type="text"
+                                required
+                                value={contact_phone}
+                                onChange={(e) => setContact_phone(e.target.value)}
+                            />
+                        </label>
 
-                        <label className="add-warehouse__form-label">Contact Position: </label>
-                        <input
-                            className="add-warehouse__form-input"
-                            placeholder="Contact Position"
-                            type="text"
-
-                            value={contact_position}
-                            onChange={(e) => setContact_position(e.target.value)}
-                        />
-                        {contact_position === "" && hasSubmit && <EmptyField />}
-
-
-                        <label className="add-warehouse__form-label">Phone Number:</label>
-                        <input
-                            className="add-warehouse__form-input"
-                            placeholder="Phone Number"
-                            type="text"
-
-                            value={contact_phone}
-                            onChange={(e) => setContact_phone(e.target.value)}
-                        />
-                        {phoneErrorMessage}
-                        
-
-
-                        <label className="add-warehouse__form-label">Email:</label>
-                        <input
-                            className="add-warehouse__form-input"
-                            placeholder="Email"
-                            type="text"
-
-                            value={contact_email}
-                            onChange={(e) => setContact_email(e.target.value)}
-                        />
-                        {contact_email === "" && hasSubmit && <EmptyField />}
-
+                        <label className="add-warehouse__form-label">Email:
+                            <input
+                                className="add-warehouse__form-input"
+                                placeholder="Email"
+                                type="text"
+                                required
+                                value={contact_email}
+                                onChange={(e) => setContact_email(e.target.value)}
+                            />
+                        </label>
 
 
                     </div>
@@ -255,12 +238,12 @@ const CreateNewWarehouse = () => {
                     </div>
 
                     {!isPending && <button className="add-warehouse__add-button">+ Add Warehouse</button>}
+                    {isPending && <a><button disabled className="add-warehouse__add-button">Addding Warehouse...</button></a>}
+                    {submitSuccess && (
+                        <div className="add-warehouse__success-message">Successfully added new warehouse. Redirecting you to the main page...</div>
+                    )}
+
                 </div>
-                {submitSuccess && (
-                    <div className="add-warehouse__success-message">Successfully added new warehouse. Redirecting you to the main page...</div>
-                )}
-
-
 
             </form>
         </section>
@@ -268,4 +251,4 @@ const CreateNewWarehouse = () => {
 
 }
 
-export default CreateNewWarehouse;
+export default EditWarehouseForm;
